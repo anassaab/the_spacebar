bedc4b78d71467c2585fa589b6a936cafe89e4a8
Anass <anass.ait.al.brimi@gmail.com>
2022-07-03 08:25:01 +0100

    Deny Access in the Controller

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -8,15 +8,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
 
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="app_comment_admin")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator): Response
     {
-
+        // $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $q = $request->query->get('q');
         $queryBuilder = $repository->getSearchWithQueryBuilder($q);
 
