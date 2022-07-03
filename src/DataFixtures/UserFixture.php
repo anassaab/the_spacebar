60c8827c4712324994ab3a5cb21f127793e81922
Anass <anass.ait.al.brimi@gmail.com>
2022-07-03 07:53:46 +0100

    add password to user entity & add password verification

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -5,9 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
 
     protected function loadData(ObjectManager $manager)
     {
@@ -15,6 +23,10 @@ class UserFixture extends BaseFixture
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->userPasswordEncoder->encodePassword(
+                $user,
+                'hello'
+            ));
             return $user;
         });
         $manager->flush();
