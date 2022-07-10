b24c3ba350fa531559248aeea341c2c1d18e2802
Anass <anass.ait.al.brimi@gmail.com>
2022-07-10 19:27:10 +0100

    The Edit Form

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -43,14 +43,28 @@ class ArticleAdminController extends AbstractController
 
 
     /**
-     * @Route("/admin/article/{id}/edit")
+     * @Route("/admin/article/{id}/edit", name="admin_article_edit")
      * @IsGranted("MANAGE", subject="article")
      */
-    public function edit(Article $article)
+    public function edit(Article $article, EntityManagerInterface $em, Request $request)
     {
-        // $this->denyAccessUnlessGranted('MANAGE', $article);
+        $form = $this->createForm(ArticleFormType::class, $article);
 
-        dd($article);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article updated');
+            return $this->redirectToRoute('admin_article_edit',[
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('article_admin/edit.html.twig', [
+            'articleForm' => $form->createView(),
+        ]);
     }
 
     /**
