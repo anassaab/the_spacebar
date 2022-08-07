5db4226c85907cbb18103fa1f3ff287c7dfe2cc2
Anass <anass.ait.al.brimi@gmail.com>
2022-08-08 00:20:10 +0100

    add sonar conf

---
+++ .scannerwork/.sonartmp/eslint-bridge-bundle/package/lib/utils/sensitive-arguments.js
@@ -0,0 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkSensitiveCall = void 0;
+const utils_1 = require("../rules/utils");
+function checkSensitiveCall(context, callExpression, sensitiveArgumentIndex, sensitiveProperty, sensitivePropertyValue, message) {
+    if (callExpression.arguments.length < sensitiveArgumentIndex + 1) {
+        return;
+    }
+    const sensitiveArgument = callExpression.arguments[sensitiveArgumentIndex];
+    const options = utils_1.getValueOfExpression(context, sensitiveArgument, 'ObjectExpression');
+    if (!options) {
+        return;
+    }
+    const unsafeProperty = utils_1.getPropertyWithValue(context, options, sensitiveProperty, sensitivePropertyValue);
+    if (unsafeProperty) {
+        context.report({
+            node: callExpression.callee,
+            message: utils_1.toEncodedMessage(message, [unsafeProperty]),
+        });
+    }
+}
+exports.checkSensitiveCall = checkSensitiveCall;
+//# sourceMappingURL=sensitive-arguments.js.map
